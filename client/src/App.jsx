56b3101cbec1a120962c92d684ca4823307369f3
AmitxParmar <amitparmar901@gmail.com>
2023-02-17 19:34:50 +0530

    create Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
       </Routes>
-
     </Router>
-
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>
+                Hello, Welcome to my Blog
+            </h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ex aliquid possimus similique nobis. Est dolores atque rem quam, enim dolor. Accusamus temporibus nam voluptas ipsa officia id sequi tempora.
+                Quibusdam nostrum labore veritatis iure ratione libero quis, excepturi, id voluptatum, reiciendis sunt animi consequuntur molestiae culpa! Illo cumque, quaerat non at placeat, similique ipsam, beatae impedit incidunt provident iste!
+            </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ex aliquid possimus similique nobis. Est dolores atque rem quam, enim dolor. Accusamus temporibus nam voluptas ipsa officia id sequi tempora.
+                Quibusdam nostrum labore veritatis iure ratione libero quis, excepturi, id voluptatum, reiciendis sunt animi consequuntur molestiae culpa! Illo cumque, quaerat non at placeat, similique ipsam, beatae impedit incidunt provident iste!
+            </p>
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ex aliquid possimus similique nobis. Est dolores atque rem quam, enim dolor. Accusamus temporibus nam voluptas ipsa officia id sequi tempora.
+                Quibusdam nostrum labore veritatis iure ratione libero quis, excepturi, id voluptatum, reiciendis sunt animi consequuntur molestiae culpa! Illo cumque, quaerat non at placeat, similique ipsam, beatae impedit incidunt provident iste!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur labore doloremque corrupti incidunt! Quas, accusamus recusandae, tenetur nesciunt quibusdam officiis totam, deserunt culpa blanditiis ipsum incidunt sint soluta hic?
+            </p>
+        </div>
+    )
+}
+
+export default Home
