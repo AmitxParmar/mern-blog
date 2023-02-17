cf07f90be95490d73d156c37dc024eb55f7984b9
AmitxParmar <amitparmar901@gmail.com>
2023-02-17 19:47:44 +0530

    complete navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
