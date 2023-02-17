64b70322922f213c33062fecb094080270c2c74e
AmitxParmar <amitparmar901@gmail.com>
2023-02-17 20:55:55 +0530

    minor changes in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Navbar from './components/Navbar'
 import NotFound from './pages/NotFound'
-function App() {
+import Article from './pages/Article'
 
+// component
+import Navbar from './components/Navbar'
+
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/articles/:name" element={<Article />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
