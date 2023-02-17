fdec20eded67f6b031d5e57f3655772639597f4a
AmitxParmar <amitparmar901@gmail.com>
2023-02-17 19:23:38 +0530

    frontend:setup router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  
+    <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+
+    </Router>
+
   )
 }
 
