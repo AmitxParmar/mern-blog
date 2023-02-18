8332440ff21d4f36411633b0d7649b123a820fff
Amit Parmar <amitparmar901@gmail.com>
2023-02-18 21:35:44 +0530

    create about page structure

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const About = () => (
+   <div>
+<h1>
+About
+</h1>
+<p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem esse, iste 
+         consectetur totam sit et neque eos eius corrupti. Accusantium quas ea id 
+         assumenda illo repellendus quasi unde nam consectetur excepturi dolorem 
+         eius repudiandae voluptates molestiae asperiores maxime ad maiores modi 
+         commodi velit nostrum cum, harum culpa aliquid? Quis, deserunt!
+
+</p>
+<p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem esse, iste 
+         consectetur totam sit et neque eos eius corrupti. Accusantium quas ea id 
+         assumenda illo repellendus quasi unde nam consectetur excepturi dolorem 
+         eius repudiandae voluptates molestiae asperiores maxime ad maiores modi 
+         commodi velit nostrum cum, harum culpa aliquid? Quis, deserunt!
+
+</p>
+<p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem esse, iste 
+         consectetur totam sit et neque eos eius corrupti. Accusantium quas ea id 
+         assumenda illo repellendus quasi unde nam consectetur excepturi dolorem 
+         eius repudiandae voluptates molestiae asperiores maxime ad maiores modi 
+         commodi velit nostrum cum, harum culpa aliquid? Quis, deserunt!
+
+</p>
+
+</div>
+)
+export default About;
