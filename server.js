3253e2613af83fd410775019858d6ee9422422d3
AmitxParmar <amitparmar901@gmail.com>
2023-02-19 22:45:26 +0530

    create backend routes to add comments

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const withDB = async (operations, res) => {
     }
 };
 
-app, get('/api/articles/:name', async (req, res) => {
+// find article by name route
+app.get('/api/articles/:name', async (req, res) => {
     withDB(async (db) => {
         const articleName = req.params.name;
         const articleInfo = await db
@@ -28,7 +29,33 @@ app, get('/api/articles/:name', async (req, res) => {
             .findOne({ name: articleName });
         res.status(200).json(articleInfo);
     }, res);
-})
+});
+
+
+// update route
+app.post("/api.articles/:name/add-comments", (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    withDB(async () => {
+        const articleInfo = await db
+            .collection('articles')
+            .findOne({ name: articleName });
+        await db.collection('articles').updateOne(
+            { name: articleName },
+            {
+                $set: {
+                    comments: articleInfo.comments.concat({ username, text }),
+                }
+            }
+        );
+        const updateArticleInfo = await db
+            .collection("articles")
+            .findOne({ name: articleName });
+
+        res.statusCode(200).json(updateArticleInfo);
+    }, res)
+});
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
 
